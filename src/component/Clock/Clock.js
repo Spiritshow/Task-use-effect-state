992781141274141001d7c336ffe53ebc3f287653
Spirit <vkozyaychev@mail.ru>
2025-04-25 18:05:22 +0500

    Add Clock

---
+++ src/component/Clock/Clock.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from "react";
+
+const Clock = () => {
+
+    const [time, setTime] = useState(new Date());
+
+    const tick = () => {
+        return(setTime(new Date()));
+    }
+
+    useEffect( () => {
+        const timer = setInterval(() => tick(), 1000);
+
+        return() => clearInterval(timer);
+    }, [])
+
+    return(
+        <div className="timeBox">
+            <h1 className="timeText">{time.toLocaleTimeString()}</h1>
+        </div>
+    )
+}
+
+export default Clock;
