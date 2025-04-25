9711e6749f3b6e186f1bc00e847f557505aa78a7
Spirit <vkozyaychev@mail.ru>
2025-04-26 02:07:12 +0500

    test

--- src/component/Clock/Clock.js
+++ src/component/Clock/Clock.js
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from "react";
 import "./Clock.css";
 
+let country = new Map();
+country.set("Tyumen", 0);
+country.set("Moscow", -2);
+country.set("Tokyo", 4);
+
 const Clock = () => {
     const [togle, setTogle] = useState(false);
     const [time, setTime] = useState(new Date());
@@ -24,7 +29,7 @@ const Clock = () => {
     return(
         <div className="timeBox">
             <h1 className="timeText">{time.toLocaleTimeString()}</h1>
-            {togle && <h3>Время {time.getHours}:{time.getMinutes}  делится на 5</h3>}
+            {togle && <h3>Время {time.getUTCHours()}:{time.getUTCMinutes()}  делится на 5</h3>}
         </div>
     )
 }
